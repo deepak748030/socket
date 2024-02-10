4dfde3faacbf2f83a67b558040291a59f671631d
deepak748030 <deepakkushwah748930@gmail.com>
2024-02-10 21:58:34 +0530

    client added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useEffect } from 'react';
+import socketIO from 'socket.io-client'
+const ENDPOINT = 'http://localhost:3000/';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const socket = socketIO(ENDPOINT, { transports: ["websocket"] })
+
+  useEffect(() => {
+    socket.on("connected", () => {
+      console.log(`connected to ${socket.id}`)
+    })
+    return socket.off("connect")
+  }, [socket])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>socket hello bhai</div>
   )
 }
 
