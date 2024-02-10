9784a85267b7ccce68c9cb46679b9739cf9aeafe
deepak748030 <deepakkushwah748930@gmail.com>
2024-02-10 21:45:40 +0530

    server file added

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const socketIO = require('socket.io');
+
+const http = require('http')
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const PORT = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+    // When a client connects, log their socket ID
+    console.log(`A client connected with socket ID: ${socket.id}`);
+
+    socket.on('disconnect', () => {
+        // When a client disconnects, log their socket ID
+        console.log(`A client disconnected with socket ID: ${socket.id}`);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
